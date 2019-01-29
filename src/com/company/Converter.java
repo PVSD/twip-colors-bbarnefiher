61bd6fc42577ac832ff7daa26075194d7b075ed0
bbarnefiher <bb222@pvsd.org>
2019-01-29 12:31:37 -0500

    end of montco day

--- src/com/company/Converter.java
+++ src/com/company/Converter.java
@@ -4,7 +4,14 @@ package com.company;
  * Created by bb222 on 1/29/19.
  */
 public class Converter {
-    public static String tripletToHex(int r, int g, int b){
-
+    public Converter(String a, String conversionType){
+        String theColor = a;
     }
+    int toHexAnswer = -1;
+    public int toHex(){
+
+
+        return toHexAnswer;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,28 @@ public class Main {
         Scanner stringReader = new Scanner(System.in);
         System.out.println("Do you want to convert an RGB triplet, hexadecimal, or a named color?\nType 'triplet', 'hexadecimal', or 'color'.");
         String userInput = stringReader.nextLine();
-        if (){
-
+        String userConvert;
+        String conversionType;
+        if (userInput.trim().equalsIgnoreCase("triplet")){
+            System.out.println("Please enter your RGB triplet in the following format:\nRRRGGGBBB\nEx: 255062001");
+            userConvert = stringReader.nextLine();
+            conversionType = "triplet";
+            Converter color = new Converter(userConvert, conversionType);
+        }
+        else if (userInput.trim().equalsIgnoreCase("hexadecimal")){
+            System.out.println("Please enter your hexadecimal code in the following format:\nFFFFFF\nEx: ACDC01");
+            userConvert = stringReader.nextLine();
+            conversionType = "hexadecimal";
+            Converter color = new Converter(userConvert, conversionType);
+        }
+        else if (userInput.trim().equalsIgnoreCase("color")){
+            System.out.println("Please enter your color name. Make sure it is one of the 140 common HTML colors.");
+            userConvert = stringReader.nextLine();
+            conversionType = "name";
+            Converter color = new Converter(userConvert, conversionType);
+        }
+        else {
+            System.out.println("Sorry, but I do not know what you mean.");
         }
     }
 }
