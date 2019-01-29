6f7b9a4a32ffd2c3cb8c316286ce733660bcdd32
bbarnefiher <bb222@pvsd.org>
2019-01-29 11:00:32 -0500

    end of class btw

---
+++ src/com/company/Converter.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by bb222 on 1/29/19.
+ */
+public class Converter {
+    public static String tripletToHex(int r, int g, int b){
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner stringReader = new Scanner(System.in);
+        System.out.println("Do you want to convert an RGB triplet, hexadecimal, or a named color?\nType 'triplet', 'hexadecimal', or 'color'.");
+        String userInput = stringReader.nextLine();
+        if (){
+
+        }
     }
 }
